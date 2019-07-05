7bbb661551797e00faf85bbd8e3827971d96056e
Yandi <yandikurniawan@gmail.com>
2019-07-05 17:15:07 +0700

    Init project

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,25 @@
+import 'text_section.dart';
+import 'package:flutter/material.dart';
+import 'image_banner.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hola'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ImageBanner("assets/images/gate.jpg"),
+          TextSection("bla bla bla", "bla 2"),
+          TextSection("bla bla bla", "bla 3"),
+          TextSection("bla bla bla", "bla 4"),
+        ],
+      )
+    );
+  }
+}
